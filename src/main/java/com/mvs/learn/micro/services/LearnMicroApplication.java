ab16e170bbd2be28946245220a1a03e79479b8f6
Siddhartha Patashani -X (spatasha - INFOSYS LIMITED at Cisco) <spatasha@cisco.com>
2017-05-19 03:27:13 -0700

    First Commit

---
+++ src/main/java/com/mvs/learn/micro/services/LearnMicroApplication.java
@@ -0,0 +1,12 @@
+package com.mvs.learn.micro.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnMicroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnMicroApplication.class, args);
+	}
+}
